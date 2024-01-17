502510fca8bf98b38f3c8cb75e2bfdc96ff14c1d
Gary Smith <garysmith193358@gmail.com>
2024-01-17 16:12:16 -0500

    generated random choice by computer, and added logic for user choice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,32 @@
 package org.example;
 
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
+        String userChoice;
         HashMap<String, String> rps = new HashMap<>();
         rps.put("r", "s");
         rps.put("s", "p");
         rps.put("p", "r");
 
+        char[] compChoices = { 'r', 'p', 's' };
+
+        while (true) {
+            System.out.println("Welcome to the Rock Paper Scissor Game!");
+            System.out.print("Please choose r, p , or s");
+
+            int compChoiceIdx = new Random().nextInt(compChoices.length);
+            char compChoice = compChoices[compChoiceIdx];
+
+            userChoice = scnr.nextLine();
+
+            if (rps.containsKey(userChoice)) {
+                break;
+            }
+        }
     }
 }
