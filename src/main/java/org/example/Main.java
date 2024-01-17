23273350af7e8b2dedfb57c2434d2e0165253668
Gary Smith <garysmith193358@gmail.com>
2024-01-17 15:52:17 -0500

    implemented hashmap and added rock, paper, scissor values to it

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        HashMap<String, String> rps = new HashMap<>();
+        rps.put("r", "s");
+        rps.put("s", "p");
+        rps.put("p", "r");
+
+    }
+}
