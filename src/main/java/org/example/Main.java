6f6974ac7b55be6c3997d13a4e0c57d93e7a3e86
Gary Smith <garysmith193358@gmail.com>
2024-01-17 16:54:56 -0500

    added logic for user to win and comp to win

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,26 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         String userChoice;
+        String compChoice;
+        boolean gameOver = false;
         HashMap<String, String> rps = new HashMap<>();
         rps.put("r", "s");
         rps.put("s", "p");
         rps.put("p", "r");
 
-        char[] compChoices = { 'r', 'p', 's' };
+        String[] compChoices = { "r", "p", "s" };
 
         while (true) {
             System.out.println("Welcome to the Rock Paper Scissor Game!");
-            System.out.print("Please choose r, p , or s");
+            System.out.print("Please choose r, p, or s");
 
             int compChoiceIdx = new Random().nextInt(compChoices.length);
-            char compChoice = compChoices[compChoiceIdx];
+            compChoice = compChoices[compChoiceIdx];
 
             userChoice = scnr.nextLine();
 
@@ -28,5 +28,29 @@ public class Main {
                 break;
             }
         }
+
+        //Draw
+
+        //User wins
+        for ( Map.Entry<String, String> entry : rps.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+
+            if (!Objects.equals(userChoice, key)) {
+                continue;
+            }
+
+            if (Objects.equals(compChoice, value)) {
+                System.out.println("You win! " + userChoice + "beats " + compChoice);
+                gameOver = true;
+            }
+        }
+
+        //Comp Wins
+        if (!gameOver) {
+            System.out.println("You Lose! " + compChoice + "beats " + userChoice);
+            gameOver = true;
+        }
+
     }
 }
